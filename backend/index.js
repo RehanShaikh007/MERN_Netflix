77325a8816e0c68082f86bcf3cdfb45a63c40a12
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2024-08-01 09:59:17 +0530

    Live Link Added

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://mern-netflix-psi.vercel.app',
     credentials: true
 }
 
